d689dab0f12f23211bf6a6c68cfbbe5aae2dab40
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-07-10 14:14:29 +0000

    Integrated technologies model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705171250) do
+ActiveRecord::Schema.define(version: 20170705173510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,14 @@ ActiveRecord::Schema.define(version: 20170705171250) do
     t.text "badge"
   end
 
+  create_table "technologies", force: :cascade do |t|
+    t.string "name"
+    t.bigint "portfolio_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["portfolio_id"], name: "index_technologies_on_portfolio_id"
+  end
+
   create_table "topics", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -71,4 +79,5 @@ ActiveRecord::Schema.define(version: 20170705171250) do
   end
 
   add_foreign_key "blogs", "topics"
+  add_foreign_key "technologies", "portfolios"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -46,3 +46,18 @@ end
 end
 
 puts "9 portfolio items created"
+
+3.times do |technology|
+  Technology.create!(
+    name: "Technology #{technology}”,
+    portfolio_id: Portfolio.last.id
+ )
+end
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
+
+puts "3 technologies created"
